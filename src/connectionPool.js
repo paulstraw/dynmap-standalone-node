145505ce9f9ff254b5119669e2d476c89a2f1e47
Paul Straw <paulstraw@paulstraw.com>
2020-04-11 23:14:38 -0700

    Add app.json for Heroku one click deploy

--- src/connectionPool.js
+++ src/connectionPool.js
@@ -3,6 +3,7 @@ import mysql from 'mysql'
 const connectionPool = mysql.createPool({
   host: process.env.MYSQL_HOST,
   user: process.env.MYSQL_USER,
+  port: process.env.MYSQL_PORT || 3306,
   password: process.env.MYSQL_PASSWORD,
   database: process.env.MYSQL_DATABASE,
   connectionLimit: parseInt(process.env.MYSQL_CONNECTION_POOL_SIZE, 10) || 3,
