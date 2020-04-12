586a174da5e3350177d6e2820aa35459b8f23296
Paul Straw <paulstraw@paulstraw.com>
2020-04-11 22:09:07 -0700

    Add CORS support

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import express from 'express'
+import cors from 'cors'
 
 import mysqlTiles from './routes/mysqlTiles'
 
 const app = express()
 const port = 3000
+
+app.use(cors())
 
 app.get('/mysql-tiles', mysqlTiles)
 
