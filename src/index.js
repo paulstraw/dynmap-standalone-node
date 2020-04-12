712ae0d172670a5aae8b87aa6f271a141506b088
Paul Straw <paulstraw@paulstraw.com>
2020-04-11 23:51:27 -0700

    I accidentally a line

--- src/index.js
+++ src/index.js
@@ -7,7 +7,6 @@ import mysqlConfiguration from './routes/mysqlConfiguration'
 import mysqlUpdate from './routes/mysqlUpdate'
 
 const app = express()
-const port = 3000
 const port = process.env.PORT || 3000
 
 app.use(cors())
